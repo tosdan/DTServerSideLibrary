c1397466fb1aa9a61703349c2446d63c7801555a
Daniele T <stdanielets@gmail.com>
2013-07-25 21:13:38 +0200

    refactor

--- WEB-INF/src/com/github/tosdan/utils/stringhe/MapFormatTypeValidator.java
+++
@@ -1,8 +0,0 @@
-package com.github.tosdan.utils.stringhe;
-
-public interface MapFormatTypeValidator
-{
-	public String getTypes();
-	
-	public String validate(String source, String type) throws MapFormatTypeValidatorException;
-}
